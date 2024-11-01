7c26e665351d886ed36dfc043844b8dcd949f54e
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-11-01 04:11:35 +0100

    pycodesstly-solved

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,23 +1,30 @@
-#!/ussr/bin/python3
+#!/usr/bin/python3
 """
-Utf8 that valid a dataset
+UTF-8 Validation for a dataset
 """
-
 
 def validUTF8(data):
-    """ Convert integer to binary string, padded to 8 bits"""
+    """Check if a dataset represents a valid UTF-8 encoding."""
+    num_bytes = 0
 
     for byte in data:
         byt_num = format(byte, '08b')
 
-        if byt_num.startswith('0'):
-            continue
-        elif byt_num.startswith('110'):
-            continue
-        elif byt_num.startswith('1110'):
-            continue
-        elif byt_num.startswith('11110'):
-            continue
+        if num_bytes == 0:
+            if byt_num.startswith('0'):
+                continue
+            elif byt_num.startswith('110'):
+                num_bytes = 1
+            elif byt_num.startswith('1110'):
+                num_bytes = 2
+            elif byt_num.startswith('11110'):
+                num_bytes = 3
+            else:
+                return False
         else:
-            return False
-    return True
+            if not byt_num.startswith('10'):
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
+
