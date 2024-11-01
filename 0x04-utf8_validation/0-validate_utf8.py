9f32715b1f31371696cb5a9c57e77384f4ceefcc
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-11-01 03:11:08 +0100

    Valid UTF-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/ussr/bin/python3
+"""
+Utf8
+"""
+
+
+def validUTF8(data):
+    num_bytes = 0
+
+    for num in data:
+        """ Convert integer to binary string, padded to 8 bits"""
+        byt_num = f'{num:08b}'
+
+        if byt_num.startswith('0'):
+            continue
+        elif byt_num.startswith('110'):
+            num_bytes = 1
+        elif byt_num.startswith('1110'):
+            num_bytes = 2
+        elif byt_num.startswith('11110'):
+            num_bytes = 3
+        else:
+            return False
+    return num_bytes == 0
