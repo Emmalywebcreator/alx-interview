e2ae61de941163bd5fde16b44df69db62e3e2be4
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-11-01 03:55:58 +0100

    Uf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,19 +6,18 @@ Utf8 that valid a dataset
 
 def validUTF8(data):
     """ Convert integer to binary string, padded to 8 bits"""
-    num_bytes = 0
 
-    for num in data:
-        byt_num = format(num_bytes, '08B')
+    for byte in data:
+        byt_num = format(byte, '08B')
 
         if byt_num.startswith('0'):
             continue
         elif byt_num.startswith('110'):
-            num_bytes = 1
+            continue
         elif byt_num.startswith('1110'):
-            num_bytes = 2
+            continue
         elif byt_num.startswith('11110'):
-            num_bytes = 3
+            continue
         else:
             return False
-    return num_bytes == 0
+    return True
