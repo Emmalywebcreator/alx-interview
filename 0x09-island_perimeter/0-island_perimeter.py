0188dd41a8dcf708826df655785872893334d5ab
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-12-06 05:16:52 +0100

    Pycodestyleemmytech@DESKTOP-PAKAUBT:~/alx-interview/0x09-island_perimeter$ pycodestyle 0-island_perimeter.pycorrected

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,25 +14,23 @@ def island_perimeter(grid):
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    perimeter += 4
+            if grid[i][j] == 1:
+                perimeter += 4
 
-                    if i > 0:
-                        if grid[i - 1][j] == 1:
-                            perimeter -=1
+                if i > 0:
+                    if grid[i - 1][j] == 1:
+                        perimeter -= 1
 
+                if i < len(grid) - 1:
+                    if grid[i + 1][j] == 1:
+                        perimeter -= 1
 
-                    if i < len(grid) - 1:
-                        if grid[i + 1][j] == 1:
-                            perimeter -= 1
+                if j > 0:
+                    if grid[i][j-1] == 1:
+                        perimeter -= 1
 
-                    if j > 0:
-                        if grid[i][j-1] == 1:
-                            perimeter -= 1
-
-                    if j < len(grid[0]) - 1:
-                        if grid[i][j+ 1] == 1:
-                            perimeter -= 1
+                if j < len(grid[0]) - 1:
+                    if grid[i][j + 1] == 1:
+                        perimeter -= 1
 
     return perimeter
-                    
