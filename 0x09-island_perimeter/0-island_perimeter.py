ba583c52405c5229b515a3b35ad5dbeccff5fe07
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-12-08 11:48:08 +0000

    function documentation added

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,11 +6,10 @@ A module that returns the perimeter of the island described in grid
 
 
 def island_perimeter(grid):
-    perimeter = 0
-
     """
     This function calculate the perimeter of an insland
     """
+    perimeter = 0
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
